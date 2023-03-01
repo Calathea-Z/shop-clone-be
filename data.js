9af7de32ef3a826b76a786c61b7af92c294510bc
Calathea-Z <zsykes21@gmail.com>
2023-02-28 19:00:29 -0500

    Add userModel and user seed data.

--- data.js
+++ data.js
@@ -1,43 +1,70 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+
+    users: [
+        {
+            name: 'Zach',
+            email: 'zsykes21@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'Johnny',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        }
+    ],
     products: [
         {
-            _id: '1',
             name: 'Nike Slim Shirt',
             slug: 'nike-slim-shirt',
-            category: 'Shirts',
             image: '/assets/shirt.jpeg',
+            brand: 'Nike',
+            category: 'Shirts',
+            description: 'high quality shirt',
             price: 120, 
             countInStock: 10,
-            brand: 'Nike',
             rating: 1.5,
             numReviews: 10,
-            description: 'high quality shirt',
         },
         {
-            _id: '2',
             name: 'Nike Slim Pants',
             slug: 'nike-slim-pants',
-            category: 'Pants',
             image: '/assets/avllogo.png',
-            price: 120, 
-            countInStock: 10,
             brand: 'Nike',
+            category: 'Pants',
+            description: 'high quality pants',
+            price: 120, 
+            countInStock: 10, 
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality pants',
+            
         },
         {
-            _id: '3',
             name: 'Adidas Slim Shirt',
             slug: 'adidas-slim-shirt',
-            category: 'Shirts',
             image: '/assets/shirt.jpeg',
+            brand: 'Adidas',
+            category: 'Shirts',
+            description: 'high quality shirt',
             price: 120, 
             countInStock: 0,
-            brand: 'Adidas',
             rating: 5.0,
             numReviews: 10,
-            description: 'high quality shirt',
+        },
+        {
+            name: 'Adidas Big Ol Shirt',
+            slug: 'adidas-big-ol-shirt',
+            image: '/assets/shirt.jpeg',
+            brand: 'Adidas',
+            category: 'Shirts',
+            description: 'A Big Ol Shirt that No One Likes :_D',
+            price: 10, 
+            countInStock: 7,
+            rating: 1.0,
+            numReviews: 2,
         },
     ]
 }
