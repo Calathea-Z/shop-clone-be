420953407e1efa412f7e4f6bb47c1e0bc59af21f
Calathea-Z <zsykes21@gmail.com>
2023-02-28 23:04:02 -0500

    Add userRoutes.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -17,14 +18,25 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded( {extended: true }))
 //SEED ROUTE
 app.use('/api/seed', seedRouter);
-//GET ALL ROUTE
+//PRODUCT ROUTE
 app.use('/api/products', productRouter);
-//GET INDIVIDUAL ROUTE
-app.get('/api/products/slug/:slug', )
+// //GET INDIVIDUAL ROUTE
+// app.get('/api/products/slug/:slug', )
+//USER ROUTE
+app.use('/api/users', userRouter);
+
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
